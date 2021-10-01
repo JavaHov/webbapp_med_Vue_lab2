6dc94a4ef0332223f0a35c4d1237c0c4f8867957
Ola Lindgård <ola.lindgard@iths.se>
2021-10-01 15:40:54 +0200

    Lite småändring i js.

--- js/activities.js
+++ js/activities.js
@@ -8,59 +8,59 @@
         <td>{{ text }}</td>
         <td>{{ name }}</td>
         <td>Ålder: {{ age }}</td>
-        <td><button v-on:click="$emit('remove')">Remove</button></td>
+        <td><button v-on:click="$emit('remove')">Ta bort</button></td>
     </tr>
  ` ,
     props: ['text', 'name', 'age'] // Här definierar jag mina egna 'properties' som jag använder
 })                          // i component där uppe...
 
     var app = new Vue({
-    el: '#app',
-    data: {
-    activities: [
-{
-    name: 'Ola',
-    id: 1,
-    age: 53,
-    text: 'Löpning'
+        el: '#app',
+        data: {
+        activities: [
+    {
+        name: 'Ola',
+        id: 1,
+        age: 53,
+        text: 'Löpning'
 
-},
-{
+    },
+    {
 
-    name: 'Anna',
-    id: 2,
-    age: 27,
-    text: 'Cykling'
+        name: 'Anna',
+        id: 2,
+        age: 27,
+        text: 'Cykling'
 
-},
-{
-    name: 'Josefin',
-    id: 3,
-    age: 36,
-    text: 'Vandring'
+    },
+    {
+        name: 'Josefin',
+        id: 3,
+        age: 36,
+        text: 'Vandring'
 
-}
+    }
     ],
-    newId: 4,
-    newName: '',
-    removeIndex: '',
-    newActivity: '',
-    newAge: ''
-},
+        newId: 4,
+        newName: '',
+        removeIndex: '',
+        newActivity: '',
+        newAge: ''
+    },
     methods: {
     addActivity: function() {
 
-    this.activities.push({
-    id: this.newId++,
-    text: this.newActivity,
-    name: this.newName,
-    age: this.newAge
-})
-},
+        this.activities.push({
+        id: this.newId++,
+        text: this.newActivity,
+        name: this.newName,
+        age: this.newAge
+    })
+    },
     switchImage: function() {
 
-    let selected = document.getElementById("activities").value
-    document.getElementById("activity_images").src = "img/" + selected + ".jpg"
-}
-}
+        let selected = document.getElementById("activities").value
+        document.getElementById("activity_images").src = "img/" + selected + ".jpg"
+    }
+    }
 })
